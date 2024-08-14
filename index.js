05d45b072fe71e567385b4791e7b790ea45064b4
AhmedShalaby <ahmedmoustfa144@gmail.com>
2024-08-14 17:54:15 +0300

    stripe added

--- index.js
+++ index.js
@@ -4,13 +4,28 @@ import { bootstrap } from "./src/modules/bootstrap.js";
 import { globalErrorHandling } from "./src/middleware/globalErrorHandling.js";
 import { appError } from "./src/utils/appError.js";
 import cors from 'cors'
+import Stripe from 'stripe';
+const stripe = new Stripe('sk_test_51Png86DWF14Y8AhBNLxOoea31qUzsEMKzN4ryyUKELy04zaPKyxLZ7vMTbhbfy3wQQqrTIj5A3TCWeZk1UIR5XHW00MXw1f9N3');
+
 // import dotenv from "dotenv"
 // dotenv.config()
 import 'dotenv/config'
+import { catchError } from "./src/middleware/catchError.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.post('/api/webhook', express.raw({type: 'application/json'}),catchError((req, res) => {
+    const sig = req.headers['stripe-signature'].toString()
+  
+    let event = stripe.webhooks.constructEvent(req.body, sig, "whsec_s7Fg5fSVAFhpGq5W4N5ALkShXT8TR7n4");
+    let checkout
+  if(event.type=='checkout.session.completed'){
+    const checkout = event.data.object;
 
+  }
+  
+    res.json({message:"success",checkout});
+  }))
 app.use(cors())
 app.use(express.json());
 app.use('/uploads',express.static('uploads'))
